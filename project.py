36dc63bb9b53a327d153b9f0a40d4fa00eb76b9f
Jonathan Burge <jburge@u.northwestern.edu>
2017-02-10 15:19:56 -0600

    finished flip names

--- project.py
+++ project.py
@@ -40,21 +40,28 @@ class BabyNames:
         result = pd.DataFrame(data = d, columns = ['Rank', 'Female', 'Male'])
         return(result)
 
-    def ChangeOfPopularity(self, fromYear, toYear, top = 10):
         #sub by year
         #get percentages by year
         #difference between two points in time
         #head and tail for increase and drops
         subFrom = self.data[self.data['Year'] == fromYear].drop(['Gender', 'State', 'Year'], 1).groupby('Name').sum()
         subFrom['Percent'] = subFrom['Count']/subFrom['Count'].sum() * 100
+        subFrom['Name'] = subFrom.index.get_level_values(0).values
+        subFrom = subFrom.reset_index(drop = True)
 
         subTo = self.data[self.data['Year'] == toYear].drop(['Gender', 'State', 'Year'], 1).groupby('Name').sum()
         subTo['Percent'] = subTo['Count']/subTo['Count'].sum() * 100
+        subTo['Name'] = subTo.index.get_level_values(0).values
+        subTo = subTo.reset_index(drop = True)
 
-        #full outer join with defaul as 0
-        #sort and horizontal slice
+        temp = subFrom.merge(subTo, how = 'outer', on = 'Name')
+        temp = temp.fillna(value = 0)
+        temp['Delta'] = -temp['Percent_x'] + temp['Percent_y']
+        temp = temp.sort_values('Delta', ascending = False)
 
-        return
+        incNames = temp.ix[:,['Name', 'Delta']].head(top).reset_index(drop = True)
+        decNames = temp.ix[:,['Name', 'Delta']].tail(top).sort_values('Delta').reset_index(drop = True)
+        return incNames, decNames
 
     def Top5NamesPerYear(self, year, sex = ''):
         n = 5
@@ -85,7 +92,45 @@ class BabyNames:
     def NamePopularityPlot(self, name, yearRange, state = '', sex = ''):
         return
     def NameFlip(self, n = 10):
-        return
+        fromYear = min(self.years)
+        toYear = max(self.years)
+
+        subFrom = self.data[self.data['Year'] == fromYear].drop(['State', 'Year'], 1).groupby(['Name', 'Gender']).sum()
+        subFrom['Name'] = subFrom.index.get_level_values(0).values
+        subFrom['Gender'] = subFrom.index.get_level_values(1).values
+        subFrom = subFrom.reset_index(drop = True)
+        subFrom['Male'] = subFrom['Gender'] == 'M'
+        subFrom['Female'] = subFrom['Gender'] == 'F'
+        subFrom = subFrom.merge(subFrom, left_on=['Name', 'Male'], right_on=['Name', 'Female'])
+        temp = subFrom['Gender_x'] == 'M'
+        subFrom = subFrom.ix[temp,:].drop(['Male_x', 'Female_x', 'Male_y', 'Female_y'], 1)
+        subFrom['Total'] = subFrom['Count_x'] + subFrom['Count_y']
+        subFrom['Prop_M'] = subFrom['Count_x']/subFrom['Total']
+        subFrom['Prop_F'] = subFrom['Count_y']/subFrom['Total']
+        subFrom = subFrom.drop(['Count_x', 'Gender_x', 'Count_y', 'Gender_y'], 1)
+        subFrom = subFrom.reset_index(drop = True)
+        ###
+        subTo = self.data[self.data['Year'] == toYear].drop(['State', 'Year'], 1).groupby(['Name', 'Gender']).sum()
+        subTo['Name'] = subTo.index.get_level_values(0).values
+        subTo['Gender'] = subTo.index.get_level_values(1).values
+        subTo = subTo.reset_index(drop = True)
+        subTo['Male'] = subTo['Gender'] == 'M'
+        subTo['Female'] = subTo['Gender'] == 'F'
+        subTo = subTo.merge(subTo, left_on=['Name', 'Male'], right_on=['Name', 'Female'])
+        temp = subTo['Gender_x'] == 'M'
+        subTo = subTo.ix[temp,:].drop(['Male_x', 'Female_x', 'Male_y', 'Female_y'], 1)
+        subTo['Total'] = subTo['Count_x'] + subTo['Count_y']
+        subTo['Prop_M'] = subTo['Count_x']/subTo['Total']
+        subTo['Prop_F'] = subTo['Count_y']/subTo['Total']
+        subTo = subTo.drop(['Count_x', 'Gender_x', 'Count_y', 'Gender_y'],1)
+        subTo = subTo.reset_index(drop = True)
+        ###
+        subDelta = subFrom.merge(subTo, on = 'Name')
+        subDelta['Delta'] = -subDelta['Prop_M_x'] + subDelta['Prop_M_y']
+        subDelta['Abs_Delta'] = abs(subDelta['Delta'])
+        subDelta = subDelta.sort_values('Abs_Delta', ascending = False)
+        subDelta = subDelta.reset_index(drop = True)
+        return(subDelta.head(n))
 
 def CreateDataFrame(pathToDataDir, df_name):
     states = ['AK', 'DC', 'IN', 'MN', 'NJ', 'RI', 'VT', 'AL', 'DE', 'KS', 'MO', 'NM', 'SC', 'WA', 'AR', 'FL', 'KY', 'MS', 'NV', 'SD', 'WI', 'AZ', 'GA', 'LA', 'MT', 'NY', 'WV', 'CA', 'HI', 'MA', 'NC', 'OH', 'TN', 'WY', 'IA', 'MD', 'ND', 'OK', 'TX', 'CO', 'ID', 'ME', 'NE', 'OR', 'UT', 'CT', 'IL', 'MI', 'NH', 'PA', 'VA']
@@ -110,4 +155,5 @@ lib = BabyNames(df_name)
 # print(lib.Count(state = 'WA', year = 1993))
 # print(lib.Top10BabyNames(state = 'WA', year = 1993))
 # print(lib.Top5NamesPerYear(year = 1993, sex = 'F'))
-print(lib.ChangeOfPopularity(fromYear = 2014, toYear = 2015, top = 10))
+#print(lib.ChangeOfPopularity(fromYear = 2014, toYear = 2015, top = 10))
+print(lib.NameFlip())
