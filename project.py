3107dbe3351228892dd752a74c232b249c4da8d7
Craig Ng <craigng@dhcp-10-105-152-206.wireless.northwestern.private>
2017-02-09 19:27:44 -0600

    did part of e

--- project.py
+++ project.py
@@ -8,6 +8,7 @@ class BabyNames:
         self.data = pd.read_pickle(file_path)
         self.states = sorted(self.data.State.unique())
         self.years = sorted(self.data.Year.unique())
+        self.genders = sorted(self.data.Gender.unique())
 
     def GetInput(self, _state, _year):
         if _state not in self.states:
@@ -38,22 +39,37 @@ class BabyNames:
         print(f)
         d = {'rank': [x+1 for x in range(0, 10)], 'female': f, 'male':m}
         result = pd.DataFrame(data = d)
-        #mnames = sub.sort('Count', ascending = False).head(n)[['Name', 'Count']]
-        #fnames = sub.Name[sub['Gender'] == 'F'].sort_values(columns = 'Count').head(n)
-        #print(mnames)
         return(result)
 
     def ChangeOfPopularity(self, fromYear, toYear, top = 10):
         return
+
     def Top5NamesPerYear(self, year, sex = ''):
-        return
+        if sex.upper() not in self.genders:
+            sex = self.genders
+        else:
+            sex = [sex]
+        if year not in self.years:
+            year = self.years
+        else:
+            year = [year]
+        sub = pd.DataFrame(self.data[self.data['Year'].isin(year) & self.data['Gender'].isin(sex)])
+        sub.drop(['Gender'])
+        sub = sub.groupby(by = ['State', 'Name', 'Year'], as_index = False).agg({'Count':sum}).sort(['State', 'Count'], ascending = [True, False]).reset_index(drop = True)
+        
+        for s in self.states:
+            
+
     def NamePopularityPlot(self, name, yearRange, state = '', sex = ''):
         return
+
     def NameFlip(self, n = 10):
         return
 
 def CreateDataFrame(pathToDataDir, df_name):
-    states = ['AK', 'DC', 'IN', 'MN', 'NJ', 'RI', 'VT', 'AL', 'DE', 'KS', 'MO', 'NM', 'SC', 'WA', 'AR', 'FL', 'KY', 'MS', 'NV', 'SD', 'WI', 'AZ', 'GA', 'LA', 'MT', 'NY', 'WV', 'CA', 'HI', 'MA', 'NC', 'OH', 'TN', 'WY', 'IA', 'MD', 'ND', 'OK', 'TX', 'CO', 'ID', 'ME', 'NE', 'OR', 'UT', 'CT', 'IL', 'MI', 'NH', 'PA', 'VA']
+    states = ['AK', 'DC', 'IN', 'MN', 'NJ', 'RI', 'VT', 'AL', 'DE', 'KS', 'MO', 'NM', 'SC', 'WA', 'AR', 'FL', 'KY', 'MS', 
+    'NV', 'SD', 'WI', 'AZ', 'GA', 'LA', 'MT', 'NY', 'WV', 'CA', 'HI', 'MA', 'NC', 'OH', 'TN', 'WY', 'IA', 'MD', 'ND', 'OK', 
+    'TX', 'CO', 'ID', 'ME', 'NE', 'OR', 'UT', 'CT', 'IL', 'MI', 'NH', 'PA', 'VA']
     columns = ['State', 'Gender', 'Year', 'Name', 'Count']
     df = pd.DataFrame(columns = columns)
     for s in states:
@@ -63,7 +79,7 @@ def CreateDataFrame(pathToDataDir, df_name):
         df = df.append(sname, ignore_index=True)
     df.to_pickle(df_name)
 
-df_name = 'names_data.pkl'
+df_name = '/Users/craigng/Documents/Winter 2017/MSIA 422/Homework/Project 1/names.pickle'
 #CreateDataFrame(r'.\\namesbystate', df_name)
 lib = BabyNames(df_name)
 #print(lib.data.size)
@@ -72,4 +88,8 @@ lib = BabyNames(df_name)
 # print(lib.states)
 # print(lib.years)
 #print(lib.Count(state = 'WA', year = 1993))
-print(lib.Top10BabyNames(state = 'WA', year = 1993))
+print(lib.Top5NamesPerYear(year = 1987, sex = 'M'))
+
+
+
+
