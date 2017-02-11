960fda8cf53323cbf92d878af3b993e92d4c7c7b
Craig Ng <craigng2017@u.northwestern.edu>
2017-02-11 16:26:20 -0600

    part 2 of the project

---
+++ mysql.py
@@ -0,0 +1,105 @@
+"""
+Question 1
+
+select *
+from country
+where population > 50000000
+order by population DESC limit 10
+
+Description: All information about the top 10 countries with populations over 50,000,000 descending by population size.
+"""
+
+"""
+Question 2
+select Continent, count(*) As Number_Countries, sum(population) As Population from country
+where population > 0
+group by Continent
+order by 1 asc
+
+Description: The total number of countries and the total population on each continent, ordered alphabetically.
+"""
+
+"""
+Question 3
+select city.Name As City, city.population
+from city
+inner join country ON city.CountryCode = country.code
+where country.code = 'USA'
+order by city.population DESC limit 10
+
+Description: Top 10 most populous cities in the United States sorted descending by population.
+"""
+
+"""
+Question 4
+select country.Name, Language, (Percentage * population) / 100
+from countrylanguage
+inner join country on countrylanguage.CountryCode = country.code
+where IsOfficial = True
+order by 3 DESC limit 10
+
+Description: Find the number of people in each country who speak the official language, then find the top 10 populations and sort descending by population.
+"""
+
+"""
+Question 5
+select Language, sum((Percentage * population) / 100)
+from countrylanguage
+inner join country ON countrylanguage.CountryCode = country.code
+group by Language
+order by 2 desc limit 5
+
+Description: Find the top 5 spoken languages in the world.
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import pymysql
+
+# create connection
+cnx = pymysql.connect(user='root', \
+      password='lowell21', \
+      host= '127.0.0.1', \
+      port=3306, \
+      db='world', \
+      autocommit=True)
+
+# import each table into a dataframe
+df_country = pd.read_sql_query('select * from Country', con=cnx)
+df_city = pd.read_sql_query('select * from city', con=cnx)
+df_countrylanguage = pd.read_sql_query('select * from countrylanguage', con=cnx)
+
+def question1():
+	sub = df_country.query('(Population > 50000000)').sort_values(['Population'], ascending = False).head(n = 10).reset_index(drop = True)
+	return(sub)
+
+def question2():
+	sub = df_country.query('(Population > 0)').groupby(by = ['Continent']).agg({'Name':'count', 'Population':'sum', }).sort_index(ascending = True)
+	sub.columns = ['Number of countries', 'Population']
+	return(sub)
+
+def question3():
+	sub = pd.merge(df_country, df_city, left_on = 'Code', right_on = 'CountryCode')
+	sub = sub[sub.Code == 'USA'][['Name_y', 'Population_y']].sort_values('Population_y', ascending = False).head(n = 10).reset_index(drop = True)
+	sub.columns = ('City', 'Population')
+	return(sub)
+
+def question4():
+	sub = pd.merge(df_country, df_countrylanguage, left_on = 'Code', right_on = 'CountryCode')
+	sub['Percentage * population / 100'] = sub.Percentage * sub.Population / 100
+	sub = sub[sub.IsOfficial == 'T'][['Name', 'Language', 'Percentage * population / 100']].sort_values('Percentage * population / 100', ascending = False).head(n = 10).reset_index(drop = True)
+	return(sub)
+
+def question5():
+	sub = pd.merge(df_country, df_countrylanguage, left_on = 'Code', right_on = 'CountryCode')
+	sub['Percentage * population / 100'] = sub.Percentage * sub.Population / 100
+	sub = sub.groupby('Language').agg({'Percentage * population / 100': 'sum'})
+	sub = sub.sort_values('Percentage * population / 100', ascending = False).head(n = 5)
+	sub.columns = (['sum(Percentage * population / 100)'])
+	return(sub)
+
+# print(question1())
+# print(question2())
+# print(question3())
+# print(question4())
+# print(question5())
