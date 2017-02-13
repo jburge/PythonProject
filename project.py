76deed2130c28b80326354b45d20ad15e75df799
Jonathan Burge <jburge@u.northwestern.edu>
2017-02-13 17:43:26 -0600

    added comments, generalized many of the functions

--- project.py
+++ project.py
@@ -2,15 +2,27 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
-
 class BabyNames:
+    ''' Library of baby names
+    This class contains a panda DataFrame containing baby name data as well as functions to obtain metrics of interest.
+    '''
+
     def __init__(self, file_path):
+        '''Initialize instance of BabyNames class
+        Parameters:
+            file_path: filepath to tkl object to load into this instance of BabyNames.
+        '''
         self.data = pd.read_pickle(file_path)
         self.states = sorted(self.data.State.unique())
         self.years = sorted(self.data.Year.unique())
         self.genders = sorted(self.data.Gender.unique())
+        self.minYear = min(self.years)
+        self.maxYear = max(self.years)
 
     def GetInput(self, _state, _year):
+        '''Used to handle user-passed parameters for several functions
+        Generally should not publically be called.
+        '''
         if _state not in self.states:
             _state = self.states
         else:
@@ -20,14 +32,29 @@ class BabyNames:
         else:
             _year = [_year]
         return _state, _year
-
+    def ForceState(self, _state):
+        return(_state.upper())
     def Count(self, state = '', year = ''):
+        '''Get count of births by state, year.
+        Parameters:
+            state: (optional parameter) Specify which state to consider in summation of births
+            year: (optional parameter) Specify which year to consider in summation of births
+        Returns: Integer value representing the count of births for the specified parameters.
+        '''
+        state = self.ForceState(state)
         state, year = self.GetInput(state, year)
         count = self.data.Count[self.data['State'].isin(state) & self.data['Year'].isin(year)].sum()
         return(count)
 
     def Top10BabyNames(self, state = '', year = ''):
+        '''Get 10 most popular baby names by state, year.
+        Parameters:
+            state: (optional parameter) Specify which state to consider in summation of births
+            year: (optional parameter) Specify which year to consider in summation of birth
+        Returns: DataFrame containing the ten most popular female and male names for the specified parameters. These names are separated into two lists (by gender) and sorted by popularity.
+        '''
         n = 10
+        state = self.ForceState(state)
         state, year = self.GetInput(state, year)
         sub = pd.DataFrame(self.data[self.data['State'].isin(state) & self.data['Year'].isin(year)])
         sub = sub.groupby(by = ['Name', 'Gender']).agg({'Count':sum}).sort_values('Count', ascending = False)
@@ -40,11 +67,18 @@ class BabyNames:
         result = pd.DataFrame(data = d, columns = ['Rank', 'Female', 'Male'])
         return(result)
 
-    def ChangeOfPopularity(self, fromYear = 2014, toYear = 2015, top = 10):
-        #sub by year
-        #get percentages by year
-        #difference between two points in time
-        #head and tail for increase and drops
+    def ChangeOfPopularity(self, fromYear = '', toYear = '', top = 10):
+        '''Get names with greatest change in popularity for a given time-frame.
+        Parameters:
+            fromYear: (optional parameter) Beginning of time-frame to consider
+            toYear: (optional parameter) End of time-frame to consider
+            top: (optional parameter) How many results to return.
+        Returns: A tuple containing two DataFrames is returned. The first of these Dataframes contains the names that have increased in popularity; the second Dataframe contains the names that have decreased in popularity.
+        '''
+        if fromYear not in self.years:
+            fromYear = self.minYear
+        if toYear not in self.years:
+            toYear = self.maxYear
         subFrom = self.data[self.data['Year'] == fromYear].drop(['Gender', 'State', 'Year'], 1).groupby('Name').sum()
         subFrom['Percent'] = subFrom['Count']/subFrom['Count'].sum() * 100
         subFrom['Name'] = subFrom.index.get_level_values(0).values
@@ -64,7 +98,13 @@ class BabyNames:
         decNames = temp.ix[:,['Name', 'Delta']].tail(top).sort_values('Delta').reset_index(drop = True)
         return incNames, decNames
 
-    def Top5NamesPerYear(self, year, sex = ''):
+    def Top5NamesPerYear(self, year = '', sex = ''):
+        '''Get the 5 most popular names for each state for a given year
+        Parameters:
+            year: What year to consider for this query
+            sex: (optional parameter) Specify if only Male or Female names should be considered
+        Returns: A DataFrame containing a row for each state. Each row lists the 5 most popular names and the count of births of children with each name respectively.
+        '''
         n = 5
         if sex not in self.genders:
             sex = self.genders
@@ -92,6 +132,21 @@ class BabyNames:
         return(df)
 
     def NamePopularityPlot(self, name = 'Jim', yearRange = (2000, 2015), state = 'IL', sex = 'M'):
+        '''Genereates a plot of the change of a name's popularity over time.
+        Parameters:
+            name: What name to consider.
+            yearRange: Tuple of two integers denoting the beginning and end of the period to consider.
+            state: Which state to consider.
+            sex: Which gender to consider
+        Returns: Although no value is returned, a plot is generated to display the name's popularity over the given time period.
+        '''
+        # state = self.ForceState(state)
+        # if state not in self.states:
+        #     state = self.states
+        # if sex not in self.genders:
+        #     sex = self.genders
+        # else:
+        #     sex = [sex]
         range_ = [int(s) for s in self.years if s >= yearRange[0] and s <= yearRange[1]]
         sub = self.data[(self.data['State'] == state) & (self.data['Gender'] == sex) & (self.data['Year'].isin(range_))].reset_index(drop = True)
         name_count = {}
@@ -116,6 +171,11 @@ class BabyNames:
         plt.show()
 
     def NameFlip(self, n = 10):
+        '''Get names that have shifted in popularity between genders.
+        Parameters:
+            n: (optional parameter) How many names to return
+        Returns: A DataFrame containing the names with a greatest shift in usage from one gender to another. This is determined by measuring the proportion of the names usage between genders at each point in time and identifying those with the larges deltas as having flipped.
+        '''
         fromYear = min(self.years)
         toYear = max(self.years)
 
@@ -157,6 +217,12 @@ class BabyNames:
         return(subDelta.head(n))
 
 def CreateDataFrame(pathToDataDir, df_name):
+    '''Generate pkl of pandas DataFrame.
+    This function loops over all input files to generate a single pandas dataframe to save as a pkl object.
+    Parameters:
+        pathToDataDir: filepath to directory containing the data-files
+        df_name: what to save the tkl object as
+    '''
     states = ['AK', 'DC', 'IN', 'MN', 'NJ', 'RI', 'VT', 'AL', 'DE', 'KS', 'MO', 'NM', 'SC', 'WA', 'AR', 'FL', 'KY', 'MS',
     'NV', 'SD', 'WI', 'AZ', 'GA', 'LA', 'MT', 'NY', 'WV', 'CA', 'HI', 'MA', 'NC', 'OH', 'TN', 'WY', 'IA', 'MD', 'ND', 'OK',
     'TX', 'CO', 'ID', 'ME', 'NE', 'OR', 'UT', 'CT', 'IL', 'MI', 'NH', 'PA', 'VA']
@@ -169,22 +235,26 @@ def CreateDataFrame(pathToDataDir, df_name):
         df = df.append(sname, ignore_index=True)
     df.to_pickle(df_name)
 
-df_name = './names_data.pkl'
-#CreateDataFrame(r'.\\namesbystate', df_name)
-lib = BabyNames(df_name)
-
 def TestFunctions():
+    '''Runs a series of tests to check expected performance.
+    '''
     print('Count Test')
-    print(lib.Count(state = 'WA', year = 1993))
+    print(lib.Count(state ='wa'))
     print('Top 10 by state and year')
-    print(lib.Top10BabyNames(state = 'WA', year = 1993))
+    print(lib.Top10BabyNames())
     print('Top 5 Per Year Test')
-    print(lib.Top5NamesPerYear(year = 1987, sex = 'M'))
+    print(lib.Top5NamesPerYear(year = 1987))
     print(lib.Top5NamesPerYear(year = 1993))
     print('Change of Popularity Test')
-    print(lib.ChangeOfPopularity(fromYear = 2014, toYear = 2015, top = 10))
+    print(lib.ChangeOfPopularity('John'))
     print('Flip Test')
     print(lib.NameFlip())
-    lib.NamePopularityPlot(name = 'Jonathan', state = 'WA')
+    print('Name Popularity Plot Test')
+    lib.NamePopularityPlot('John')
 
+
+df_name = './names_data.pkl'
+
+#CreateDataFrame(r'.\\namesbystate', df_name)
+lib = BabyNames(df_name)
 TestFunctions()
